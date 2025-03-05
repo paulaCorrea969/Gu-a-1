88ad8e2d0915c1ac0f64f9c060f61c0f083181b5
paulaCorrea969 <pcorrea66315@universidadean.edu.co>
2025-03-05 16:18:59 -0500

    Create tienda.java

---
+++ tienda.java
@@ -0,0 +1,10 @@
+public class Tienda {
+    private String nombre;
+    private String direccion;
+    private String telefono;
+
+    public Tienda(String nombre, String direccion, String telefono) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
