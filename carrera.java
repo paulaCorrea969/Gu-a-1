78a3803c6481508dd89e888b07d79ffc10086c1f
paulaCorrea969 <pcorrea66315@universidadean.edu.co>
2025-03-05 16:24:04 -0500

    Create carrera.java

---
+++ carrera.java
@@ -0,0 +1,12 @@
+public class Carrera {
+    private String nombre;
+    private int creditosTotales;
+    private int semestres;
+    private String nivel;
+
+    public Carrera(String nombre, int creditosTotales, int semestres, String nivel) {
+        this.nombre = nombre;
+        this.creditosTotales = creditosTotales;
+        this.semestres = semestres;
+        this.nivel = nivel;
+    }
