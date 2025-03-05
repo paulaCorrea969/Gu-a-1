0e56862cbd900ad7928e684a1b80e91ee5bba626
paulaCorrea969 <pcorrea66315@universidadean.edu.co>
2025-03-05 16:25:31 -0500

    Create curso.java

---
+++ curso.java
@@ -0,0 +1,14 @@
+public class Curso {
+    private int codigo;
+    private String nombre;
+    private int creditos;
+    private int salon;
+    private String edificio;
+
+    public Curso(int codigo, String nombre, int creditos, int salon, String edificio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.creditos = creditos;
+        this.salon = salon;
+        this.edificio = edificio;
+    }
