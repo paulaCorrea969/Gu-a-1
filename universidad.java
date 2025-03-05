6c6f389bd1588d6962c106ca3d269d09f66d2d3f
paulaCorrea969 <pcorrea66315@universidadean.edu.co>
2025-03-05 16:21:36 -0500

    Create universidad.java

---
+++ universidad.java
@@ -0,0 +1,10 @@
+public class Universidad {
+    private String nombre;
+    private String rector;
+    private String ciudad;
+
+    public Universidad(String nombre, String rector, String ciudad) {
+        this.nombre = nombre;
+        this.rector = rector;
+        this.ciudad = ciudad;
+    }
