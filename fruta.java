7c2144ef89fad4ae6e396b2c90ef5fe2b758a434
paulaCorrea969 <pcorrea66315@universidadean.edu.co>
2025-03-05 16:17:30 -0500

    Update fruta.java

--- fruta.java
+++ fruta.java
@@ -1,19 +1,4 @@
-// ˅
-import java.util.*;
-import java.text.*;
-
-// ˄
-
-public class Fruta {
-    // ˅
-    
-    // ˄
-
-    // ˅
-    
-    // ˄
-}
-
-// ˅
-
-// ˄
+public class Fruta extends Producto {
+    public Fruta(int codigo, String nombre, String fechaExpiracion, String fabricante, int cantidadInventario, double precioUnitario) {
+        super(codigo, nombre, "FRUTA", fechaExpiracion, fabricante, cantidadInventario, precioUnitario);
+    }
