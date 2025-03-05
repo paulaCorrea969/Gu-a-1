4c50c2871c6ab0e94536e222bc23543bf6c5b88b
paulaCorrea969 <pcorrea66315@universidadean.edu.co>
2025-03-05 16:23:01 -0500

    Create persona.java

---
+++ persona.java
@@ -0,0 +1,12 @@
+public class Persona {
+    private String cedula;
+    private String nombre;
+    private String fechaNacimiento;
+    private String lugarNacimiento;
+
+    public Persona(String cedula, String nombre, String fechaNacimiento, String lugarNacimiento) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.lugarNacimiento = lugarNacimiento;
+    }
