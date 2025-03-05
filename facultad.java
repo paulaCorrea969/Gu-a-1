675d82654394ea22307f4a7a986d7cdcda8681b6
paulaCorrea969 <pcorrea66315@universidadean.edu.co>
2025-03-05 16:22:09 -0500

    Create facultad.java

---
+++ facultad.java
@@ -0,0 +1,8 @@
+public class Facultad {
+    private String nombre;
+    private int codigo;
+
+    public Facultad(String nombre, int codigo) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+    }
