e44c7b45f5efd703740867daa730b37600c9dd76
paulaCorrea969 <pcorrea66315@universidadean.edu.co>
2025-03-05 16:17:08 -0500

    Update enlatado.java

--- enlatado.java
+++ enlatado.java
@@ -1,19 +1,4 @@
-// ˅
-import java.util.*;
-import java.text.*;
-
-// ˄
-
-public class Enlatado {
-    // ˅
-    
-    // ˄
-
-    // ˅
-    
-    // ˄
-}
-
-// ˅
-
-// ˄
+public class Enlatado extends Producto {
+    public Enlatado(int codigo, String nombre, String fechaExpiracion, String fabricante, int cantidadInventario, double precioUnitario) {
+        super(codigo, nombre, "ENLATADO", fechaExpiracion, fabricante, cantidadInventario, precioUnitario);
+    }
